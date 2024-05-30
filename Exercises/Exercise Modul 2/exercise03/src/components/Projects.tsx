bca86e925f7779e313c9c80ce2514f130c114365
christov99 <christoval999@gmail.com>
2024-05-30 11:31:35 +0700

    init exercise

--- Exercises/Exercise Modul 2/exercise03/src/components/Projects.tsx
+++ Exercises/Exercise Modul 2/exercise03/src/components/Projects.tsx
@@ -7,12 +7,12 @@ const Projects: React.FC = () => {
       <h1>Projects</h1>
       <div className="project-list">
         <div className="project">
-          <h2>Project 1</h2>
-          <p>Project description here.</p>
+          <h2>Front-End Web Development</h2>
+          <p>Using the HTML, CSS, Javascript, and others to create eye-catching websites.</p>
         </div>
         <div className="project">
-          <h2>Project 2</h2>
-          <p>Project description here.</p>
+          <h2>Back-End Web Development</h2>
+          <p>Using programs such as MySQL to manage datas better.</p>
         </div>
       </div>
     </section>
